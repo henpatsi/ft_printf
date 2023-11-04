182a97d58b03948d48e798499e2891d28140400d
henpatsi <49587535+henpatsi@users.noreply.github.com>
2023-11-04 14:23:27 +0200

    basic tests for each conversion

--- tests.c
+++ tests.c
@@ -13,9 +13,9 @@
 #include <stdio.h>
 #include "ft_printf.h"
 
-int test_noargs(void)
+int test_no_conv(void)
 {
-    printf("\nno args = (\"hello\")\n");
+    printf("\nargs = (\"hello\")\n");
     printf("hello");
     printf(" || ");
     fflush(stdout);
@@ -24,13 +24,101 @@ int test_noargs(void)
     return (0);
 }
 
-int test_chararg(void)
+int test_c_conv(void)
 {
-    printf("\nchar arg = (\"hello mr %%c\", 'A')\n");
-    printf("hello mr %c", 'A');
+    printf("\nargs = (\"Mr %%c\", 'F')\n");
+    printf("Mr %c", 'F');
     printf(" || ");
     fflush(stdout);
-    ft_printf("hello mr %c", 'A');
+    ft_printf("Mr %c", 'F');
+    printf("\n");
+    return (0);
+}
+
+int test_p_conv(void)
+{
+    printf("\nargs = (\"address: %%p\", \"address of this\")\n");
+    printf("address: %p", "address of this");
+    printf(" || ");
+    fflush(stdout);
+    ft_printf("address: %p", "address of this");
+    printf("\n");
+    return (0);
+}
+
+int test_s_conv(void)
+{
+    printf("\nargs = (\"hello %%s\", \"mom\")\n");
+    printf("hello %s", "mom");
+    printf(" || ");
+    fflush(stdout);
+    ft_printf("hello %s", "mom");
+    printf("\n");
+    return (0);
+}
+
+int test_d_conv(void)
+{
+    printf("\nargs = (\"n. %%d\", 1)\n");
+    printf("n. %d", 1);
+    printf(" || ");
+    fflush(stdout);
+    ft_printf("n. %d", 1);
+    printf("\n");
+    return (0);
+}
+
+int test_i_conv(void)
+{
+    printf("\nargs = (\"n. %%i\", 2)\n");
+    printf("n. %i", 2);
+    printf(" || ");
+    fflush(stdout);
+    ft_printf("n. %i", 2);
+    printf("\n");
+    return (0);
+}
+
+int test_u_conv(void)
+{
+    printf("\nargs = (\"n. %%u\", -1)\n");
+    printf("n. %u", -1);
+    printf(" || ");
+    fflush(stdout);
+    ft_printf("n. %u", -1);
+    printf("\n");
+    return (0);
+}
+
+int test_x_conv(void)
+{
+    printf("\nargs = (\"n. %%x\", 42)\n");
+    printf("n. %x", 42);
+    printf(" || ");
+    fflush(stdout);
+    ft_printf("n. %x", 42);
+    printf("\n");
+    return (0);
+}
+
+int test_X_conv(void)
+{
+    printf("\nargs = (\"n. %%X\", 42)\n");
+    printf("n. %X", 42);
+    printf(" || ");
+    fflush(stdout);
+    ft_printf("n. %X", 42);
+    printf("\n");
+    return (0);
+}
+
+int test_percent_conv(void)
+{
+    printf("\nargs = (\"100 %%%%\")\n");
+    printf("100 %%");
+    printf(" || ");
+    fflush(stdout);
+    ft_printf("100 %%");
     printf("\n");
     return (0);
 }
@@ -38,8 +126,16 @@ int test_chararg(void)
 int main(void)
 {
     printf("\nprintf || ft_printf\n");
-    test_noargs();
-    test_chararg();
+    test_no_conv();
+    test_c_conv();
+    test_s_conv();
+    test_p_conv();
+    test_d_conv();
+    test_i_conv();
+    test_u_conv();
+    test_x_conv();
+    test_X_conv();
+    test_percent_conv();
     printf("\n");
     return (0);
 }

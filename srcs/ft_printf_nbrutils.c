93b1f8ea993a028c57ceb1b6181ffd3f08f5d41d
Henri Patsi <hpatsi@c1r1p7.hive.fi>
2023-11-10 13:20:25 +0200

    fixed norm errors

--- srcs/ft_printf_nbrutils.c
+++ srcs/ft_printf_nbrutils.c
@@ -6,7 +6,7 @@
 /*   By: hpatsi <hpatsi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 09:27:16 by hpatsi            #+#    #+#             */
-/*   Updated: 2023/11/10 09:27:16 by hpatsi           ###   ########.fr       */
+/*   Updated: 2023/11/10 13:19:57 by hpatsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,12 +47,13 @@ int	ft_i_putunbr(unsigned int n)
 
 int	ft_i_puthex(unsigned int n, int use_capitals)
 {
-    int	written;
-    int	dif;
+	int	written;
+	int	dif;
 
-    written = 0;
+	written = 0;
 	if (n >= 16)
-		written = ft_add_check_error(written, ft_i_puthex(n / 16, use_capitals));
+		written = ft_add_check_error(written,
+				ft_i_puthex(n / 16, use_capitals));
 	if (use_capitals)
 		dif = ('A' - '9' - 1);
 	else
@@ -61,28 +62,28 @@ int	ft_i_puthex(unsigned int n, int use_capitals)
 	if (n > '9')
 		n += dif;
 	written = ft_add_check_error(written, ft_i_putchar(n));
-    return (written);
+	return (written);
 }
 
 static int	i_putlonghex(unsigned long n)
 {
-    int	written;
+	int	written;
 
-    written = 0;
+	written = 0;
 	if (n >= 16)
 		written = ft_add_check_error(written, i_putlonghex(n / 16));
 	n = (n % 16) + '0';
 	if (n > '9')
 		n += ('a' - '9' - 1);
 	written = ft_add_check_error(written, ft_i_putchar(n));
-    return (written);
+	return (written);
 }
 
 int	ft_i_putaddr(void *ptr)
 {
-    int		written;
+	int		written;
 
 	written = ft_i_putstr("0x");
 	written = ft_add_check_error(written, i_putlonghex((unsigned long) ptr));
-    return (written);
+	return (written);
 }
